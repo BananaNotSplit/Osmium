544132c58b82acf3c9279d4d3747b21b4eae1f6e
BananaNotSplit <98235195+BananaNotSplit@users.noreply.github.com>
2026-03-29 09:05:44 -0400

    Removed old module loading system, and replaced it with a dynamic one.

--- src/osmium/index.ts
+++ src/osmium/index.ts
@@ -1,26 +1,57 @@
-import { ActivityType, Client, Events } from "discord.js";
 import ModuleLoader from "./ModuleSystem/Loader";
-import Leveling from "./Modules/Leveling";
-import ColonThree from "./Modules/ColonThree";
+import { readdirSync } from "fs";
+import { join } from "path";
+import ModuleImplementation from "./ModuleSystem/Implementation";
+import { ActivityType, Client, Events } from "discord.js";
 
-const loader = new ModuleLoader()
-loader.use(Leveling)
-loader.use(ColonThree)
-
-//#region [ Main ]
 const data = require("../../data/global.json") as { token: string }
-const client = new Client({ intents: [loader.requiredIntents()] })
+const loader = new ModuleLoader()
 
-process.on("SIGINT", () => {
-	console.group("Cleanup")
-	loader.cleanup(client)
-	process.exit(0)
-})
+type Constructor = new (...args: any[]) => any
 
-client.login(data.token)
+function isvalidModule(module: Function & { prototype: any }): module is ModuleImplementation {
+	return Object.hasOwn(module, "friendlyName") //only required field for ModuleImplementation
+}
 
-client.on(Events.ClientReady, client => {
-	client.user.setActivity({ name: `Osmium - Modules: ${loader.moduleTypes.length}`, type: ActivityType.Custom})
-	loader.startup(client)
-})
-//#endregion
+async function loadModules(modulesDir: string = join(__dirname, "Modules")) {
+	const files = readdirSync(modulesDir).filter(
+		(f) => f.endsWith(".js")
+	);
+
+	for (const file of files) {
+		const filePath = join(modulesDir, file);
+		const module = await import(filePath);
+
+		for (const [exportName, exported] of Object.entries(module)) {
+			if (!exported) continue
+			if (exportName !== "default") continue // each file should only output one module, as default
+			if (typeof exported !== "object") continue
+
+			if (!Object.hasOwn(exported, exportName)) continue
+			//@ts-ignore We assure the existence of this field above.
+			const module = exported[exportName]
+
+			if (typeof module !== "function") continue
+			if (!isvalidModule(module)) continue
+
+			console.debug(module)
+			loader.use(module)
+		}
+	}
+}
+
+
+async function main() {
+	await loadModules()
+	console.log(`Loading ${loader.moduleTypes.length} modules`)
+	const client = new Client({ intents: [loader.requiredIntents()] })
+
+	client.on(Events.ClientReady, (truthfulClient) => {
+		truthfulClient.user.setActivity({ name: `Osmium - Modules: ${loader.moduleTypes.length}`, type: ActivityType.Custom})
+		loader.startup(truthfulClient)
+	})
+
+	await client.login(data.token)
+}
+
+main()
